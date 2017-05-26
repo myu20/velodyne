55a12767e6009a7a0b4990c3cb7c2173c17f9a6d
Megan Yu <mtyu227@gmail.com>
2017-05-26 13:23:53 -0400

    Changed default calibration file

--- velodyne_pointcloud/src/lib/rawdata.cc
+++ velodyne_pointcloud/src/lib/rawdata.cc
@@ -83,7 +83,7 @@ namespace velodyne_rawdata
 
         // have to use something: grab unit test version as a default
         std::string pkgPath = ros::package::getPath("velodyne_pointcloud");
-        config_.calibrationFile = pkgPath + "/params/64e_utexas.yaml";
+        config_.calibrationFile = pkgPath + "/params/VLP-16.yaml";
       }
 
     ROS_INFO_STREAM("correction angles: " << config_.calibrationFile);
